9c527ada951ccff963b4e680ec90282e9956c93f
Duncan McAlester <dmcalester@gmail.com>
2022-01-15 21:49:33 -0800

    Added live data support to operations
    Currently this seems like a neat feature, but should
    consider what the implications of having two users able to
    edit data at the same time.
    
    At minimum the changes should be sent live rather than wholesale and the UX should incorporate an indication the doc is
    being edited by someone else at the same time.
    
    Writing this out I think this should probably be deferred
    until a later date

--- src/routes/[routers]/[routerId].svelte
+++ src/routes/[routers]/[routerId].svelte
@@ -17,10 +17,24 @@
 				console.log(router)
 		});
 		
-		const operationsSnapshot = await getDocs(collection(db, "routers", $page.params.routerId, "operations"));
+		
+		// This DOES subscribe to live data. Need to consider the UX and performance
+		// implications of the snapshot
+		const operationsCol = collection(db, "routers", $page.params.routerId, "operations");
+		const queryAllOperations = query(operationsCol);
+		onSnapshot(queryAllOperations, (querySnapshot) => {
+			operations = querySnapshot.docs.map(doc => {
+				return { id: doc.id, ...doc.data() }
+			})
+		});
+		
+		
+		// This does not subscribe to live data. Need to consider the UX and performance
+		// implications of the snapshot
+		/* const operationsSnapshot = await getDocs(collection(db, "routers", $page.params.routerId, "operations"));
 		operations = operationsSnapshot.docs.map((doc) => {
 			return { id: doc.id, ...doc.data() };
-		})
+		}) */
 	})
 	
 	

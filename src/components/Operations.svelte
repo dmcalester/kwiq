2859e91b79fc4bde020ba3e7da8b6113b72705da
Duncan McAlester <dmcalester@gmail.com>
2022-01-30 11:17:10 -0800

    Abstracted Operations to its own component

---
+++ src/components/Operations.svelte
@@ -0,0 +1,49 @@
+<script>
+	import Operation from './Operation.svelte';
+	
+	export let operations;
+		
+		let newOperation = {
+			number: '',
+			description: ''
+		};
+		
+		
+		const addOperation = () => {
+			console.log('add op');
+		}
+	
+</script>
+
+{ #if operations && operations.length }
+	<header class="operations__header">
+		<h1 class="operations__meta">{operations.length} Operations</h1>
+	</header>
+		
+	<ul class="list">
+	<!-- Operation Header //-->
+		<li class="line-item list-header list-header--operation">
+			<div class="operation__number">#</div>
+			<div class="operation__desscription">Description</div>
+			<div class="time operation__time">Time</div>
+		</li>
+
+		<!-- Operations //-->
+		{ #each operations as operation }
+	
+		<Operation  
+			id={operation.id}
+			bind:time={operation.time}
+			bind:number={operation.number}
+			bind:description={operation.description}
+			bind:elements={operation.elements} />
+	
+		{ /each }
+
+		<!-- New Operation //-->
+		<li class="line-item line-item--new">
+			<input type="text" class="" bind:value="{newOperation.description}" >
+			<button class="action-item" on:click={addOperation}>+</button>
+		</li>
+	</ul>
+{ /if }
--- src/routes/[routers]/[routerId].svelte
+++ src/routes/[routers]/[routerId].svelte
@@ -4,7 +4,7 @@
 	import { collection, doc, addDoc, onSnapshot, query, orderBy } from "firebase/firestore"; 
 	import { db } from '../../fb.js'
 	
-	import  Operation  from '../../components/Operation.svelte';
+	import  Operations  from '../../components/Operations.svelte';
 	
 	import '../../css/routers.css';
 	
@@ -46,18 +46,18 @@
 	
 	
 	/* OPERATION CRUD */
-	const addOperation = async() => {		
-		const opRef = await addDoc(operationsCol($page.params.routerId), {
-			number: newOperation.number,
-			description: newOperation.description,
-			createdAt: new Date(),
-			time: 0,
-			order: operations.length + 1,
-			elements: []
-		});
-		
-		newOperation = {};
-	}
+	// const addOperation = async() => {		
+	// 	const opRef = await addDoc(operationsCol($page.params.routerId), {
+	// 		number: newOperation.number,
+	// 		description: newOperation.description,
+	// 		createdAt: new Date(),
+	// 		time: 0,
+	// 		order: operations.length + 1,
+	// 		elements: []
+	// 	});
+	// 	
+	// 	newOperation = {};
+	// }
 	
 </script>
 
@@ -68,38 +68,7 @@
 	</div>
 	
 	<section id="operations">
-	{ #if operations && operations.length }
-		<header class="operations__header">
-			<h1 class="operations__meta">{operations.length} Operations</h1>
-		</header>
-			
-		<ul class="list">
-		<!-- Operation Header //-->
-			<li class="line-item list-header list-header--operation">
-				<div class="operation__number">#</div>
-				<div class="operation__desscription">Description</div>
-				<div class="time operation__time">Time</div>
-			</li>
-	
-			<!-- Operations //-->
-			{ #each operations as operation }
-		
-			<Operation  
-				id={operation.id}
-				bind:time={operation.time}
-				bind:number={operation.number}
-				bind:description={operation.description}
-				bind:elements={operation.elements} />
-		
-			{ /each }
-	
-			<!-- New Operation //-->
-			<li class="line-item line-item--new">
-				<input type="text" class="" bind:value="{newOperation.description}" >
-				<button class="action-item" on:click={addOperation}>+</button>
-			</li>
-		</ul>
-	{ /if }
-
+		<Operations 
+			bind:operations={operations} />
 	</section>
 </div>

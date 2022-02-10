91b6e4fffd65853d9b9d5f0b45390e5d84db1af7
Duncan McAlester <dmcalester@gmail.com>
2022-02-09 16:26:21 -0800

    Initial functional ability to add time to an element via defaults
    It’s pretty ugly, but time is set using the default value of motion studies.
    Need to add tests to this to validate the math. Could use a major refactor
    and it’s not currently saving.

--- src/components/Element.svelte
+++ src/components/Element.svelte
@@ -6,38 +6,60 @@
 	export let description;
 	export let time;
 	export let distance = 1;
-	export let weight = 1;
-	export let motion = { type: 'Grasp', option: 'Easy' };
+	export let weight = 2;
+	export let motion = { type: 'Grasp', option: 'Easy', code: 'A1' };
 	export let [frequency, compoundFrequency] = [1, 1];
 
-	let directTime = 0;
-	let options = {};
+	let timeRange = [0];
+	let directInput = 0;
 
-	// let _weight;
-	// let ephemeralTime = time;
+	const WEIGHT_LIMIT = 2;
 
-	// derived time
-	// $: time = ephemeralTime * (frequency * compoundFrequency) + _weight;
-	$: time = time;
 	$: motionMenu = dataLibrary.find((el) => el.label === motion.type);
+	$: weightTime = weight - WEIGHT_LIMIT > 2 ? weight - WEIGHT_LIMIT : 0;
+	$: time =
+		((timeRange.find((int) => int >= distance) || timeRange[timeRange.length - 1]) + weightTime) *
+		(frequency * compoundFrequency);
 
 	const dispatch = createEventDispatcher();
 
 	const updateDistanceIndex = () => {
-		// ephemeralTime = dataLibrary[motion.type].options[motion.option].values.find(
-		// 	(n) => n > distance
-		// );
+		// time = timeRange.find((n) => n >= distance) || timeRange[timeRange.length];
 	};
 
 	const updateWeight = () => {
 		// TODO: account for imperial measurements, this is metric
-		// TODO: remove magic number of 2
-		// _weight = weight - 2;
+	};
+
+	const updateDirectTime = () => {
+		timeRange[0] = directInput;
+	};
+
+	/* 
+		TODO: There should be a more reactive way to handle updating the two 
+					select menu elements, but for now this is sufficient
+		
+		TODO: Need to adapt this to handle an additional nested modified for
+		UAS getAndPlace
+	*/
+	const updateOption = () => {
+		const optionMenu = motionMenu.options.find((el) => el.label === motion.option);
+		if (optionMenu.values) {
+			motion.code = optionMenu.code;
+			timeRange = optionMenu.values;
+		} else if (optionMenu.modifier) {
+			// then do stuff for those edge cases
+		}
 	};
 
 	const updateMotion = () => {
-		if (motion.type !== 'Direct Input') {
+		if (motion.type === 'Direct Input') {
+			motion.code = 'DI';
+			delete motion.option;
+		} else {
 			motion.option = motionMenu.options[0].label;
+			motion.code = motionMenu.options[0].code;
+			timeRange = motionMenu.options[0].values;
 		}
 	};
 
@@ -50,7 +72,8 @@
 
 <li class="line-item">
 	<div class="line-item row">
-		<pre>{JSON.stringify(motion.option, undefined, 2)}</pre>
+		<pre>{JSON.stringify(motion, undefined, 2)}</pre>
+		<pre>{timeRange} | {weightTime}</pre>
 	</div>
 	<div class="line-item row">
 		<div
@@ -68,18 +91,24 @@
 		<!-- The primary action type //-->
 
 		<select bind:value={motion.type} on:change={updateMotion}>
-			{#each dataLibrary as menu}
-				<option value={menu.label}>{menu.label}</option>
+			{#each dataLibrary as motion}
+				<option value={motion.label}>{motion.label}</option>
 			{/each}
 			<option value="Direct Input">Direct Input</option>
 		</select>
 
 		<!-- If direct input //-->
-		{#if motion.type === 'directInput'}
-			<input type="number" min="0" step="0.1" bind:value={directTime} on:change={updateElement} />
+		{#if motion.type === 'Direct Input'}
+			<input
+				type="number"
+				min="0"
+				step="0.1"
+				bind:value={directInput}
+				on:change={updateDirectTime}
+			/>
 			<!-- If not direct input //-->
 		{:else}
-			<select bind:value={motion.option}>
+			<select bind:value={motion.option} on:change={updateOption}>
 				{#each motionMenu.options as option}
 					<option value={option.label}>{option.label}</option>
 				{/each}
@@ -87,27 +116,14 @@
 
 			<!-- The modified action //-->
 
-			<!-- distance if //-->
-			<label for="weight">Distance</label>
-			<input
-				name="distance"
-				type="number"
-				min="0"
-				step="0.1"
-				bind:value={distance}
-				on:change={updateDistanceIndex}
-			/>
-
-			<!-- weight if //-->
-			<label for="weight">Weight</label>
-			<input
-				name="weight"
-				type="number"
-				min="0"
-				step="0.1"
-				bind:value={weight}
-				on:change={updateWeight}
-			/>
+			{#if dataLibrary.find((el) => el.label === motion.type).distance}
+				<label for="weight">Distance</label>
+				<input name="distance" type="number" min="0" step="0.1" bind:value={distance} />
+			{/if}
+			{#if dataLibrary.find((el) => el.label === motion.type).weight}
+				<label for="weight">Weight</label>
+				<input name="weight" type="number" min="0" step="0.1" bind:value={weight} />
+			{/if}
 		{/if}
 	</div>
 	<button class="action-item" on:click={dispatch('delete', { id: id })}>×</button>
